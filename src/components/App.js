a1953fe260023b13f910d530d967b7885d6db844
ti-abhishek <ti.abhishek15@gmail.com>
2022-12-08 16:04:15 +0530

    n

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,19 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+	    this.state = {
+	    text: "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy",
+		    click : 0
+	    
+	    }
 	};
 
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+	<button id="click" onClick={() => this.setState({count: this.state.count + 1 })}>Button</button>
+    <h6 id="para">{this.state.text}</h6>
     		</div>
     	);
     }
